fe713cdb2886f6510d46e5b255c1c2798ec64c51
Matko <tkoma999@gmail.com>
2023-04-05 15:03:02 +0200

    Add functionalityochanging div colours on mousedown.

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
 function makeDivs(numDivs) {
-    const containerDiv = document.createElement("div");
-    containerDiv.className = "child-divs-container";
-        for (let i = 0; i < numDivs; ++i){
-            const childDiv = document.createElement('div');
-            childDiv.className="child-divs";
-            containerDiv.appendChild(childDiv)    
-            }
-        return containerDiv;
+  const containerDiv = document.createElement("div");
+  containerDiv.className = "child-divs-container";
+  for (let i = 0; i < numDivs; ++i) {
+    const childDiv = document.createElement("div");
+    childDiv.className = "child-divs";
+    containerDiv.appendChild(childDiv);
+  }
+  return containerDiv;
 }
-const numDivs = 126;
+
+const numDivs = 256;
 const containerDiv = makeDivs(numDivs);
-document.getElementById("main-content").appendChild(containerDiv);   
+document.getElementById("main-content").appendChild(containerDiv);
+
+
+let mouseDown = false;
+
+document.querySelectorAll(".child-divs").forEach((div) => {
+  div.addEventListener("mousedown", () => {
+    mouseDown = true;
+  });
+  div.addEventListener("mousemove", () => {
+    if (mouseDown) {
+      div.style.backgroundColor = "black";
+    }
+  });
+  div.addEventListener("mouseup", () => {
+    mouseDown = false;
+  });
+  div.addEventListener("click", () => {
+    div.style.backgroundColor = "black";
+  });
+});
